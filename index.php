e4466aa94434110e3c3bfef298a15da47d013837
AwHFR <kevin@alberca.fr>
2015-03-02 15:47:24 +0100

    New tree of directory

--- index.php
+++ index.php
@@ -1,38 +1,22 @@
 <!DOCTYPE html>
-<html lang="en">
-
+<html lang="fr">
 <head>
-
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="description" content="">
-    <meta name="author" content="">
-
-    <title>Bare - Start Bootstrap Template</title>
-
+    <meta name="author" content="Groupe_7">
+    <title>TakeCar - Rush Groupe 7</title>
     <!-- Bootstrap Core CSS -->
     <link href="assets/css/bootstrap.css" rel="stylesheet">
-
-    <!-- Custom CSS -->
-    <style>
-        body {
-            padding-top: 70px;
-            /* Required padding for .navbar-fixed-top. Remove if using .navbar-static-top. Change if height of navigation changes. */
-        }
-    </style>
-
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
-
 </head>
-
 <body>
-
 <!-- Navigation -->
 <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
     <div class="container">
@@ -64,7 +48,6 @@
     </div>
     <!-- /.container -->
 </nav>
-
 <!-- Page Content -->
 <div class="container">
 
@@ -73,8 +56,8 @@
             <h1>A Bootstrap Starter Template</h1>
             <p class="lead">Complete with pre-defined file paths that you won't have to change!</p>
             <ul class="list-unstyled">
-                <li>Bootstrap v3.3.1</li>
-                <li>jQuery v1.11.1</li>
+                <li>Bootstrap v3.3.2</li>
+                <li>jQuery v1.11.2</li>
             </ul>
         </div>
     </div>
@@ -82,10 +65,8 @@
 
 </div>
 <!-- /.container -->
-
-<!-- jQuery Version 1.11.1 -->
+<!-- jQuery Version 1.11.2 -->
 <script src="assets/js/jquery-1.11.2.min.js"></script>
-
 <!-- Bootstrap Core JavaScript -->
 <script src="assets/js/bootstrap.js"></script>
 
