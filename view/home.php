4e9ef4ea26dac1767054052b5ffb62210bd130f1
AwHFR <kevin@alberca.fr>
2015-03-03 15:13:08 +0100

    Flat UI

--- view/home.php
+++ view/home.php
@@ -1,1 +1,34 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr-FR">
+<head>
+    <meta charset="utf-8">
+    <title>Home - takeCar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="../assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="../assets/css/flat-ui.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="../assets/img/favicon.ico">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+    <script src="../assets/js/vendor/html5shiv.js"></script>
+    <script src="../assets/js/vendor/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="../assets/css/style.css" />
+</head>
+<body>
+    <nav class="navbar navbar-inverse" role="navigation">
+
+    </nav>
+    <!-- /.nav -->
+    <div class="container">
+        <h1>Hello, world!</h1>
+    </div>
+    <!-- /.container -->
+
+
+<!-- jQuery (necessary for Flat UI's JavaScript plugins) -->
+<script src="../assets/js/vendor/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="../assets/js/vendor/video.js"></script>
+<script src="../assets/js/flat-ui.min.js"></script>
+</body>
+</html>
