737e3d6dd487e82e71699bd23b22dc29c0399efd
AwHFR <kevin@alberca.fr>
2015-03-03 16:19:13 +0100

    add routing

--- index.php
+++ index.php
@@ -1,3 +1,50 @@
 <?php
+    $routing = [
+        'home' => [
+            'controller' => 'home',
+            'secure' => false,
+        ],
 
+        'login' => [
+            'controller' => 'login',
+            'secure' => false,
+        ],
+    ];
+
+    if(isset($_GET['action'])){
+        $action = $_GET['action'];
+        if(!isset($routing[$action])){
+            $action = 'errors/404';
+        }
+    } else {
+        $action = 'home';
+    }
 ?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Home - takeCar</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/css/flat-ui.min.css" rel="stylesheet">
+    <link rel="shortcut icon" href="assets/img/favicon.ico">
+    <!-- HTML5 shim, for IE6-8 support of HTML5 elements. All other JS at the end of file. -->
+    <!--[if lt IE 9]>
+    <script src="assets/js/vendor/html5shiv.js"></script>
+    <script src="assets/js/vendor/respond.min.js"></script>
+    <![endif]-->
+    <link rel="stylesheet" href="assets/css/style.css" />
+</head>
+<body>
+<?php
+    require_once('view/'.$action.'.php');
+?>
+
+<!-- jQuery (necessary for Flat UI's JavaScript plugins) -->
+<script src="assets/js/vendor/jquery.min.js"></script>
+<!-- Include all compiled plugins (below), or include individual files as needed -->
+<script src="assets/js/vendor/video.js"></script>
+<script src="assets/js/flat-ui.min.js"></script>
+</body>
+</html>
---
+++ view/login.php
@@ -0,0 +1,5 @@
+coucou
+
+
+<a class="link" href="?action=home">Home</a>
+<a class="link" href="?action=login">test</a>
