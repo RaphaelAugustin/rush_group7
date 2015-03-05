977879efe2dc4e63bbd8609b5201696f55508567
AwHFR <kevin@alberca.fr>
2015-03-05 21:23:07 +0100

    implemented contact page

--- Src/view/contact.php
+++ Src/view/contact.php
@@ -18,4 +18,44 @@
         </div>
         <button type="submit" class="btn-reset btn-primary">Envoyer</button>
     </form>
+    <?php
+    @$lastname = htmlentities($_POST['lastname']);
+    @$nickname = htmlentities($_POST['nickname']);
+    @$object = htmlentities($_POST['object']);
+    @$userEmail = htmlentities($_POST['userEmail']);
+    @$emailContent = htmlentities($_POST['emailContent']);
+
+
+    $destinataire = 'localhost@awh.fr'; // Déclaration de l'adresse de destination.
+
+    if (!preg_match("#^[a-z0-9._-]+@(hotmail|live|msn).[a-z]{2,4}$#", $userEmail))
+    {
+        $passage_ligne = "\r\n";
+    }
+    else
+    {
+        $passage_ligne = "\n";
+    }
+
+    //=====Création du header de l'e-mail.
+    $header = "From: ".$userEmail.$passage_ligne;
+    $header.= "Reply-to: ".$userEmail.$passage_ligne;
+    $header.= "MIME-Version: 1.0";
+    $header.= "Content-Type: multipart/alternative;".$passage_ligne;
+    //==========
+
+    //=====Ajout du message au format texte.
+    $message = $passage_ligne.$emailContent.$passage_ligne;
+    //==========
+
+    //=====Envoi de l'e-mail.
+    if(strlen($emailContent) > 0){
+        mail($destinataire,$object,$message,$header);
+        if(mail($destinataire,$object,$message,$header) === true){
+            echo 'Message envoyé';
+        }else {
+            echo 'Une erreur est survenue lors de l\'envoi du mail';
+        }
+    }
+    ?>
 </div>
