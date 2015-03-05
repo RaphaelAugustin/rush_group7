69bc34bff22834d9c3c22b59336a8fe0b7d4d33c
RaphaelAugustin <taf.augustin@gmail.com>
2015-03-05 12:08:38 +0100

    osef

--- index.php
+++ index.php
@@ -7,11 +7,7 @@ session_start();
 //require autoloader
     require_once "vendor/autoload.php";
 //connect to database
-<<<<<<< HEAD
     $PDO =new PDO('mysql:host=localhost;dbname=noxduck', 'root', '',[
-=======
-    $PDO =new PDO('mysql:host=localhost;dbname=mydb', 'root', ' ',[
->>>>>>> e957b3771ad9b4744f81949b87f6f6021f83bd40
         PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION
     ]);
 //    $user = new Src\Model\User($PDO);
